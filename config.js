547e057e541f5fa4f2e1ef8dc9434a03efa8d8fa
AbuzarFX <70717700+AbuzarFX@users.noreply.github.com>
2021-02-05 00:17:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+exports.TOKEN = 'NzUzMTI3NDkzODU0Mjk4MTYz.X1hqog.ly6zezr6KyA3_p0vBmj1iOOGxnc';
+
+exports.DBL_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc2ODE1NTc0MzgxOTM5OTE2OSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjA3MzQ4NTk5fQ.28pLbJmN_2UrUtEYvexXRWpE-Fridrj5QMoBS27Ke4M';
+
+exports.PREFIX = 'k!';
+
+exports.GOOGLE_API_KEY = 'AIzaSyC5S5zY4od9NagaN9fMReG5wJkY1IMz0rY';
+
+exports.GENIUS_API_KEY = 'Z2uCm3aOXnHaDgLWGUYMP17iPkwG4hoqJGEhJ5ygPwIu6AB6JDYmhvNkocWwlQyF';
+
+exports.yandex_API = 'pdct.1.1.20201208T112314Z.aae9b6fa8f0c54c2.baec1dc05439ba1da699e13f6d1283883a3e8abb';
+
+exports.news_API = '959d66afcfff48dcb0ffc39701918760';
+
+exports.giphy_API = 'M4bQC9ICgrrs0T86O2mZU0ejoL2dsdOH';
+
+exports.AME_API = '6f1553e8261df658e6defb976d719538c4f375cf722638be7ec32bb9f5df0bb8c299c1d1a4a3cd320223a48ac867fbb1e9dac37aa7184d1c54d6fafba46de4e3';
+
+exports.blague_API = 'AhFsr.MefuV87VUlWrDmYnLLDSBVB.JtC_zkLwuoypw4TIM-Bt8Yvg5b9oINTZnz';
