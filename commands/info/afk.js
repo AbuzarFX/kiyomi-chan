a8a73e5d920d89e54a724f6c5283e22786d15050
Syed Abuzar <70717700+AbuzarFX@users.noreply.github.com>
2021-03-31 14:51:36 +0530

    Create afk.js

---
+++ commands/info/afk.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js")
+
+const { afk } = require("../../Collection")
+
+module.exports = {
+    config: {
+        name: "afk",
+        description: "Used to set AFK status."
+    },
+    run: async (bot, message, args) => {
+
+        const reason = args.join(" ") || 'No reason specified.';
+
+        afk.set(message.author.id, [Date.now(), reason])
+
+        message.lineReplyNoMention({ embed: {
+            color: "#ff0000",
+            description: `Your AFK status has been set to: \`${reason}\``
+        }})
+
+    }
+}
